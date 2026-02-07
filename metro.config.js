94e13a8d844d3b5109c1eb7429209136e24c823f
Henry Tran <henrykhoiminh@gmail.com>
2026-02-07 08:25:37 -0500

    prep for rive integration

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Add .riv files as assets for Rive animations
+config.resolver.assetExts.push('riv');
+
 // Simple performance optimizations
 config.transformer = {
   ...config.transformer,
